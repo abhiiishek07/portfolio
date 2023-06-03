9c0b794993bcded2867efd0341d39f07758ac9bc
Abhishek <abhi885990@gmail.com>
2023-06-03 13:07:52 +0530

    Created Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+function Navbar() {
+  const [crossBar, setCrossBar] = useState(false);
+  const links = [
+    {
+      id: 0,
+      link: "Home",
+    },
+    {
+      id: 1,
+      link: "About",
+    },
+    {
+      id: 2,
+      link: "Projects",
+    },
+    {
+      id: 3,
+      link: "Technologies",
+    },
+    {
+      id: 4,
+      link: "Contact",
+    },
+  ];
+  return (
+    <div className="flex justify-between items-center w-full h-20 text-white fixed bg-black px-4 ">
+      <div>
+        <h1 className="text-5xl font-signature ml-2">Abhishek</h1>
+      </div>
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => {
+          return (
+            <li
+              key={id}
+              className="px-4 cursor-pointer font-medium text-gray-500 hover:scale-105 duration-200  "
+            >
+              {link}
+            </li>
+          );
+        })}
+      </ul>
+      <div
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+        onClick={() => setCrossBar(!crossBar)}
+      >
+        {crossBar ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+      {crossBar && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800">
+          {links.map(({ id, link }) => {
+            return (
+              <li
+                key={id}
+                className="px-4 cursor-pointer font-medium py-6 text-4xl "
+              >
+                {link}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default Navbar;
